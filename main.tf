e2fe8b9a2558ddcd26dd9681928a4b59257f13a3
chandanjataha <chandanjataha@gmail.com>
2025-06-29 21:38:32 +0530

    rg added in code

---
+++ main.tf
@@ -0,0 +1,16 @@
+terraform {
+    required_providers {
+      azurerm = {
+        source = "hashicorp/azurerm"
+        version = "4.33.0"
+      }
+    }
+}
+provider "azurerm" {
+    features {}
+    subscription_id = "f0cb494b-a275-45f4-895c-33af48053122"
+}
+resource "azurerm_resource_group" "rg" {
+name = "chandanrg3"
+location = "west us"
+}
